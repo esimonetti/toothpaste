fd2e16db11602da70f289e0dc1e718b2ca7b2e53
Enrico Simonetti <enrico.simonetti@gmail.com>
2022-05-11 09:29:40 +1000

    Updated version number to accept contribution, and added darwin

--- build/generatePhar.php
+++ build/generatePhar.php
@@ -3,15 +3,26 @@
 // Enrico Simonetti
 // enricosimonetti.com
 
-$tmpDir = dirname(__FILE__) . '/tmp';
-if (!is_dir($tmpDir)) {
-    mkdir($tmpDir);
+$pharIni = ini_get('phar.readonly');
+if ($pharIni) {
+    echo PHP_EOL . 'Please make sure the setting phar.readonly is set to false' . PHP_EOL;
+    exit(1);
 }
 
+$tmpDir = dirname(__FILE__) . '/tmp';
+if (is_dir($tmpDir)) {
+    exec('rm -rf ' . $tmpDir);
+}
+
+mkdir($tmpDir);
 chdir($tmpDir);
 exec('composer -n require esimonetti/toothpaste *');
 
-$buildRoot = dirname(__FILE__) . '/../phar/';
+$buildRoot = dirname(__FILE__) . '/../phar';
+if (!is_dir($buildRoot)) {
+    mkdir($buildRoot);
+}
+
 $fileName = 'toothpaste.phar';
 $phar = new Phar($buildRoot . '/' . $fileName, 0, $fileName);
 $phar->buildFromDirectory($tmpDir . '/vendor');
@@ -21,4 +32,4 @@ set_include_path(get_include_path() . PATH_SEPARATOR . 'phar://' . __FILE__);
 require_once 'esimonetti/toothpaste/bin/toothpaste.php'; __HALT_COMPILER();
 ");
 
-exec('cd .. && rm -rf ' . $tmpDir);
+echo PHP_EOL . 'Phar generated: ' . $buildRoot . '/' . $fileName . PHP_EOL . PHP_EOL;
